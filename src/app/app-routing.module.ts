54cd4766b4f33dd261216d0dabe78248827268be
sd ss <95736922+SS2199@users.noreply.github.com>
2024-05-03 15:53:14 +0530

    Create gallery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'gallery',
+    loadChildren: () => import('./gallery/gallery.module').then( m => m.GalleryPageModule)
+  },
 ];
 
 @NgModule({
